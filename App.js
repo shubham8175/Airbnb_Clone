72b1831041c061c47ea4695fe1afdcca7a0130d2
shubham8175 <shubham7868175@gmail.com>
2023-09-14 11:27:46 +0530

    Hello

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Entypo from 'react-native-vector-icons/Entypo'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Airbnb_Clone</Text>
+<Entypo name='instagram-with-circle'/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
